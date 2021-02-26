ea143ce2d78e432c376c0c3d92397cc31149130d
Jess Dever <jessicadever1@gmail.com>
2021-02-26 13:19:42 -0600

    through phase 5

--- Program.cs
+++ Program.cs
@@ -1,12 +1,72 @@
-﻿System.Console.Write("Who would you like to say hello to? ");
+﻿using System;
 
-string name = System.Console.ReadLine();
+Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine();
+MooseSays("H I, I'M  E N T H U S I A S T I C !");
+MooseSays("I really am enthusiastic");
 
-if (string.IsNullOrWhiteSpace(name))
+bool isTrue = MooseAsks("Is Canada real?");
+Console.WriteLine(isTrue);
+
+void MooseSays(string message)
 {
-    System.Console.WriteLine("Fine, don't say 'hello'!");
+    Console.WriteLine($@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
 }
-else
+
+bool MooseAsks(string question)
 {
-    System.Console.WriteLine($"Hello, {name}!");
+
+    /* first, we're asking the user a question, and allow only y or n as a response*/
+
+    Console.Write($"{question} (Y/N): ");
+
+    /*This waits for a response and makes sure that all answers are in lowercase*/
+
+    string answer = Console.ReadLine().ToLower();
+
+    /*If an answer other than y or n is entered, it restates the question*/
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    /*If answer comes back yes, true is rendered; if answer comes back no, false is rendered*/
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
